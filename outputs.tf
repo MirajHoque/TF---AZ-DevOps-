822143b52c261706a05cdfc79d08e7d9636790ac
Ashraful Hoque Miraj <ahmiraj@catec.ca>
2023-08-24 19:01:14 +0600

    change varible name

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,6 @@ output "Project_ID" {
   value = azuredevops_project.terraform_ado_project.id
 }
 
-output "Project_URL" {
+output "Project_Name" {
   value = azuredevops_project.terraform_ado_project.name
 }
